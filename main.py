8c7821b94a179d0d07d184a7227b0d419b7332d8
nitsundon <114671003+nitsundon@users.noreply.github.com>
2025-09-13 15:58:46 +0530

    Sunburst Plot Trial

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import pandas as pd
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://niteshsunildongre_db_user:CAVSdrZTnNMKZGSk@cluster0.cwebq8g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# Create a new client and connect to the server
+import certifi
+client = MongoClient(uri, tlsCAFile=certifi.where())
+print(client.admin.command("ping"))
 
-
-print("hello world!!")
+client.get_database()
