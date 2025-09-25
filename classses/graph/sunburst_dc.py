37d4140059285a27e240c6c4ae06da04e098e164
nitsundon <114671003+nitsundon@users.noreply.github.com>
2025-09-25 21:38:43 +0530

    Changes: Changes in home layout

---
+++ classses/graph/sunburst_dc.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import plotly.graph_objs as go
+import dash
+from dash import html
+
+
+class SunburstGraph:
+    def __init__(self):
+        pass
+
+    def DCGraph(self):
+        pass
+
